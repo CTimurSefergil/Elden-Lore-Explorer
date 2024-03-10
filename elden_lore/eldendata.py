458ec3f1e300ae172c411346cd79e02f498a607f
Timur Sefergil <sefergil29@gmail.com>
2024-03-10 19:35:41 +0300

    Improved code for the data

--- elden_lore/eldendata.py
+++ elden_lore/eldendata.py
@@ -1,9 +1,8 @@
 from bs4 import BeautifulSoup
 import requests, csv
 
-"""
 #Code for getting descriptions from items#
-
+"""
 urlItem = 'https://rankedboost.com/elden-ring/weapons/'
 pageItem = requests.get(urlItem)
 soupItem = BeautifulSoup(pageItem.text, 'html.parser').find_all('div', class_ = 'tier-list-object-name-table-css')[::2]
@@ -15,9 +14,12 @@ for i in range(len(soupItem)):
     soupDescription = BeautifulSoup(pageDescription.text, 'html.parser').find_all('em')
 
     with open('eldendata.csv', 'a+', newline='') as data:
+        data.write('\n')
         for i in range(len(soupDescription)):
-            data.write(soupDescription[i].get_text())
-            data.write('\n')
+            if soupDescription[i].get_text() != 'Int':
+                data.write(soupDescription[i].get_text())
+                data.write('\n')
+
 """
 """
 #Code for getting item names#
@@ -30,4 +32,5 @@ with open('weapons.csv', 'w', newline='') as data:
     for i in range(len(soup)):
         data.write(soup[i].get_text())
         data.write('\n')
+        
 """
